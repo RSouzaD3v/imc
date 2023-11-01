780f6d273ff1c8f4fc90773600c7b834aca3ef6e
Th3K1ngDev <rafaelf.souzan02@gmail.com>
2023-11-01 04:01:08 -0300

    Primeira

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,11 +33,11 @@ function getNivelImc(imc){
 "Obesidade Grau 2", "Obesidade Grau 3"];
 
     if(imc < 18.5) return nivel[0];
-    if(imc >= 18.5 && imc <= 24.9) return nivel[1];
-    if(imc >= 25 && imc <= 29.9) return nivel[3];
-    if(imc >= 30 && imc <= 34.9) return nivel[4];
-    if(imc >= 35 && imc <= 39.9) return nivel[5];
-    if(imc >= 40) return nivel[6];
+    if(imc >= 18.5 && imc <= 24.999) return nivel[1];
+    if(imc >= 25 && imc <= 29.999) return nivel[2];
+    if(imc >= 30 && imc <= 34.999) return nivel[3];
+    if(imc >= 35 && imc <= 39.999) return nivel[4];
+    if(imc >= 40) return nivel[5];
 }
 
 function setResultado(msg){
