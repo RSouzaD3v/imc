e7a8d8c4f5a96284382ca633fffe7970ff67b57b
Th3K1ngDev <rafaelf.souzan02@gmail.com>
2023-11-01 03:46:27 -0300

    Primeira

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,10 @@ function getNivelImc(imc){
 
     if(imc < 18.5) return nivel[0];
     if(imc >= 18.5 && imc <= 24.9) return nivel[1];
-    if(imc <= 25 && imc <= 29.9) return nivel[3];
-    if(imc <= 30 && imc <= 34.9) return nivel[4];
-    if(imc <= 35 && imc <= 39.9) return nivel[5];
-    if(imc <= 40) return nivel[6];
+    if(imc >= 25 && imc <= 29.9) return nivel[3];
+    if(imc >= 30 && imc <= 34.9) return nivel[4];
+    if(imc >= 35 && imc <= 39.9) return nivel[5];
+    if(imc >= 40) return nivel[6];
 }
 
 function setResultado(msg){
